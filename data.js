f2f2a70ba6e887f0a5240a29aa2dd98a3aae51b5
360estate <vidnoe2010@gmail.com>
2025-04-12 21:49:22 +0300

    Upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-img_20250412_160020_00_039",
-      "name": "IMG_20250412_160020_00_039",
+      "id": "0--30",
+      "name": "Малоголосківська 30а",
       "levels": [
         {
           "tileSize": 256,
@@ -37,7 +37,7 @@ var APP_DATA = {
           "yaw": 1.3800399461655974,
           "pitch": 0.0489765801640889,
           "rotation": 0,
-          "target": "1-img_20250412_155853_00_036"
+          "target": "1-2--4-"
         }
       ],
       "infoHotspots": [
@@ -50,8 +50,8 @@ var APP_DATA = {
       ]
     },
     {
-      "id": "1-img_20250412_155853_00_036",
-      "name": "IMG_20250412_155853_00_036",
+      "id": "1-2--4-",
+      "name": "2 Сеція 4 поверх",
       "levels": [
         {
           "tileSize": 256,
@@ -86,7 +86,325 @@ var APP_DATA = {
           "yaw": 2.401364447032627,
           "pitch": 0.15059214537823706,
           "rotation": 0,
-          "target": "0-img_20250412_160020_00_039"
+          "target": "0--30"
+        },
+        {
+          "yaw": 0.6973876947159319,
+          "pitch": 0.1363578989305978,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Прихожа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4345508699249834,
+        "pitch": -0.03816391511783479,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35730573961942724,
+          "pitch": 0.2585738336892245,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.4617626904718186,
+          "pitch": 0.04878263168827779,
+          "rotation": 0,
+          "target": "1-2--4-"
+        },
+        {
+          "yaw": 0.422217036437317,
+          "pitch": 0.4027166787894778,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": 0.3691910884732863,
+          "pitch": 0.10264273038718841,
+          "rotation": 0,
+          "target": "5--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "С/В",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.698298151661996,
+        "pitch": 0.013963336635065104,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.866250732730757,
+          "pitch": 0.10372948406762106,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1",
+      "name": "Прихожа 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.5575337564612672,
+        "pitch": 0.10868409124905654,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7331587079566448,
+          "pitch": 0.4597651214630254,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.7199235589939885,
+          "pitch": 0.15065901751393618,
+          "rotation": 0,
+          "target": "1-2--4-"
+        },
+        {
+          "yaw": -1.9052993730355183,
+          "pitch": 0.1523342572079649,
+          "rotation": 0,
+          "target": "5--1"
+        },
+        {
+          "yaw": 1.9452498121526727,
+          "pitch": 0.17044011455950425,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.970360391166107,
+          "pitch": 0.3955739469424735,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--1",
+      "name": "Кімната 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.30552110835161983,
+          "pitch": 0.1802363005005283,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": 2.4482767564448933,
+          "pitch": 0.44229628328455206,
+          "rotation": 0,
+          "target": "6--11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--11",
+      "name": "Кімната 1/1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8203521836254986,
+        "pitch": 0.33838671404479825,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07858961297615785,
+          "pitch": 0.10164475805580864,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": -0.6512503376603025,
+          "pitch": 0.4188794672791474,
+          "rotation": 0,
+          "target": "5--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "Вітальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.8365882939325324,
+        "pitch": 0.004506540876608511,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9954334065880506,
+          "pitch": 0.4216220175600256,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": 2.80859945288093,
+          "pitch": 0.10386282141336167,
+          "rotation": 0,
+          "target": "5--1"
         }
       ],
       "infoHotspots": []
@@ -96,7 +414,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
